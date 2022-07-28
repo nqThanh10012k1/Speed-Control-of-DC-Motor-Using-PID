75d496dc83a2c48855ad3a691cf80ec345970c4a
ThanhNguyen <thanhnq.10012k1@gmail.com>
2022-07-28 19:12:50 +0700

    Completed

---
+++ SPEED_CONTROL_DC_MOTOR/Debug/Src/main.d
@@ -0,0 +1,27 @@
+Src/main.o: ../Src/main.c \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cm4.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_usart.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h \
+ D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/stm32f401re.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cm4.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_usart.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_tim.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_exti.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_syscfg.h:
+D:/STM32/STM32_CMCIS/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/misc.h:
